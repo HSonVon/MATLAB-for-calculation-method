e8c92524cc8eb5f6b20a7a3d8bdd93fa45419fc4
Trần Hồng Sơn <114810118+HSonVon@users.noreply.github.com>
2024-04-14 12:39:38 +0700

    Delete thong_ke_diem.m

--- thong_ke_diem.m
+++
@@ -1,6 +0,0 @@
-function [soluong, diem_thapnhat, diem_caonhat, diem_trungbinh] = thong_ke_diem(diem)
-    soluong = length(diem);
-    diem_thapnhat = min(diem);
-    diem_caonhat = max(diem,[],'all');
-    diem_trungbinh = round(mean(diem),2);
-end
